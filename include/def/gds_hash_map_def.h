717fec07427ec5dc875ff9c32442faceb9b9b541
novak-stevanovic <novakst24@gmail.com>
2025-02-11 19:33:57 +0100

    Added some basic code for the future GDSHashMap.

--- include/def/gds_hash_map_def.h
+++ include/def/gds_hash_map_def.h
@@ -5,15 +5,21 @@
 #error "Do not include directly."
 #endif // __GDS_HASHMAP_DEF_ALLOW__
 
+#include <stdbool.h>
+
 #define __GDS_VECTOR_DEF_ALLOW__
 #include "gds_vector_def.h"
 
 struct GDSHashMap
 {
-    struct GDSVector data;
+    struct GDSVector _data;
 
+    size_t _entry_count;
+    size_t _key_data_size, _value_data_size;
     size_t (*_hash_func)(void* data, size_t max_val);
-    void (*_on_removal_func)(void*);
+    bool (*_key_compare_func)(void* key1, void* key2);
+    void (*_on_key_removal_func)(void*);
+    void (*_on_value_removal_func)(void*);
 };
 
 #endif // _GDS_HASH_MAP_DEF_H_
--- include/gds_hash_map.h
+++ include/gds_hash_map.h
@@ -1,5 +1,8 @@
-#ifndef _GDS_HASH_MAP_H
-#define _GDS_HASH_MAP_H
+#ifndef _GDS_HASH_MAP_H_
+#define _GDS_HASH_MAP_H_
+
+#include "gds.h"
+#include <stddef.h>
 
 #ifdef GDS_ENABLE_OPAQUE_STRUCTS
 struct GDSHashMap;
@@ -10,9 +13,22 @@ struct GDSHashMap;
 
 typedef struct GDSHashMap GDSHashMap;
 
-void gds_hash_map_init(GDSHashMap* hash_map);
+#define GDS_HASH_MAP_ERR_BASE 3000
+// #define GDS_VEC_ERR_VEC_EMPTY 3001
+#define GDS_HASH_MAP_ERR_MALLOC_FAIL 3002
+#define GDS_HASH_MAP_ERR_REALLOC_FAIL 3003
+#define GDS_HASH_MAP_ERR_INIT_FAIL 3004
+// #define GDS_VEC_ERR_CHUNK_GEN_FUNC_FAIL 3005 // Occurs when vector->_get_next_chunk_size_func returns 0
 
-GDSHashMap* gds_hash_map_create();
+gds_err gds_hash_map_init(GDSHashMap* hash_map,
+        size_t key_data_size, size_t value_data_size,
+        size_t (*hash_func)(void* data, size_t max_val),
+        void (*on_removal_func)(void*));
 
-#endif // _GDS_HASH_MAP_H
+GDSHashMap* gds_hash_map_create(size_t key_data_size, size_t value_data_size,
+        size_t (*hash_func)(void* data, size_t max_val),
+        void (*on_removal_func)(void*));
 
+gds_err gds_hash_map_set(GDSHashMap* hash_map, void* key, void* value);
+
+#endif // _GDS_HASH_MAP_H_
